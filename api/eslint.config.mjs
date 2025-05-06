7cfe9915223de8ce9842a44f45866802d4771f22
ariel.henryson <ariel.henryson@gmail.com>
2025-05-06 14:14:04 +0300

    wip

--- api/eslint.config.mjs
+++ api/eslint.config.mjs
@@ -30,6 +30,7 @@ export default tseslint.config(
       '@typescript-eslint/no-floating-promises': 'warn',
       '@typescript-eslint/no-unsafe-argument': 'warn',
       '@/semi': ['error', 'never'],
+      'comma-dangle': ['error', 'always-multiline'],
     },
   },
 );
--- api/src/libs/db/config/config.validation.spec.ts
+++ api/src/libs/db/config/config.validation.spec.ts
@@ -1,41 +1,41 @@
 import { validationSchema } from './config.validation'
 
 describe('validationSchema', () => {
-    it('should validate all env', () => {
-        const schema = validationSchema()
-        const envVars = {
-            NODE_ENV: 'test',
-            MONGODB_URI: 'mongodb://localhost:27017/test',
-            JWKS_ADDRESS: 'some url',
-            REDIS_HOST: 'localhost',
-            REDIS_PORT: 6379,
-        }
-    
-        const res = schema.validate(envVars)
+  it('should validate all env', () => {
+    const schema = validationSchema()
+    const envVars = {
+      NODE_ENV: 'test',
+      MONGODB_URI: 'mongodb://localhost:27017/test',
+      JWKS_ADDRESS: 'some url',
+      REDIS_HOST: 'localhost',
+      REDIS_PORT: 6379,
+    }
 
-        expect(res.error).toBeUndefined()
-    })
+    const res = schema.validate(envVars)
 
-    it('should throw an error when NODE_ENV is invalid', () => {
-        const schema = validationSchema()
-        const envVars = {
-            NODE_ENV: 'invalid',
-            MONGODB_URI: 'mongodb://localhost:27017/test',
-        }
+    expect(res.error).toBeUndefined()
+  })
 
-        const res = schema.validate(envVars)
+  it('should throw an error when NODE_ENV is invalid', () => {
+    const schema = validationSchema()
+    const envVars = {
+      NODE_ENV: 'invalid',
+      MONGODB_URI: 'mongodb://localhost:27017/test',
+    }
 
-        expect(res.error).not.toBeNull()
-    })
+    const res = schema.validate(envVars)
 
-    it('should throw an error when MONGODB_URI is missing', () => {
-        const schema = validationSchema()
-        const envVars = {
-            NODE_ENV: 'test',
-        }
+    expect(res.error).not.toBeNull()
+  })
 
-        const res = schema.validate(envVars)
+  it('should throw an error when MONGODB_URI is missing', () => {
+    const schema = validationSchema()
+    const envVars = {
+      NODE_ENV: 'test',
+    }
 
-        expect(res.error).not.toBeNull()
-    })
+    const res = schema.validate(envVars)
+
+    expect(res.error).not.toBeNull()
+  })
 })
