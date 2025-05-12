78056900ea436ef79245f218c92be46002168beb
ariel.henryson <ariel.henryson@gmail.com>
2025-05-12 11:18:23 +0300

    wip

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,13 +1,16 @@
 import './App.css'
-
 import { RouterProvider } from 'react-router'
 import { router } from './routes/AppRoutes'
+import { AuthProvider } from 'react-oidc-context'
+import appConfig from './config/app.config'
+
+console.log(appConfig)
 
 const App = () => {
   return (
-    <div>
+    <AuthProvider>
       <RouterProvider router={router} />
-    </div>
+    </AuthProvider>
   )
 }
 
---
+++ app/src/config/app.config.ts
@@ -0,0 +1,8 @@
+const appConfig = {
+  apiUrl: import.meta.env.VITE_API_URL,
+  // Add other shared config here
+}
+
+console.log(appConfig)
+
+export default appConfig
